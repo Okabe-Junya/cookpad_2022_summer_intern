8a533e814a5b87cf6cad390091bbf5b714d1b058
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-09-15 15:48:30 +0900

    css

--- prod/app/controllers/home_controller.rb
+++ prod/app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def index
+    @productions = Production.all
   end
 end
--- prod/config/routes.rb
+++ prod/config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'home/index'
   root 'home#index'
+  resources :home, only: [:index]
+  resources :productions
+  get 'home/index'
+  get '/productions/new', to: 'productions#new'
 end
